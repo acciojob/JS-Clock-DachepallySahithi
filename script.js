5b05546876b725dde70d0c9253fdf204fce6a8a4
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-02-17 09:10:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+function updateClock() {
+    const now = new Date();
+
+    const second = now.getSeconds();
+    const minute = now.getMinutes();
+    const hour = now.getHours();
+
+    const secondDeg = (second / 60) * 360;
+    const minuteDeg = (minute / 60) * 360 + (second / 60) * 6; 
+    const hourDeg = ((hour % 12) / 12) * 360 + (minute / 60) * 30; 
+
+    const secondHand = document.querySelector('.second-hand');
+    const minuteHand = document.querySelector('.min-hand');
+    const hourHand = document.querySelector('.hour-hand');
+
+    secondHand.style.transform = `rotate(${secondDeg}deg)`;
+    minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
+    hourHand.style.transform = `rotate(${hourDeg}deg)`;
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
